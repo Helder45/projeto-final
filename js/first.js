8d08c7dfb5279a6be767546aea39850db283783e
Helder45 <helder.pereira@estudante.ifms.edu.br>
2023-05-29 16:58:44 -0400

    adding validation in sign in form

--- js/first.js
+++
@@ -1,5 +0,0 @@
-console.log('Hello World!')
-
-let idade = 26
-
-console.log(idade)
---
+++ js/validar.js
@@ -0,0 +1,13 @@
+var senha = document.getElementById("senha"),
+confirmaSenha = document.getElementById("confirmaSenha");
+
+function validaSenha() {
+if (senha.value != confirmaSenha.value) {
+  confirmaSenha.setCustomValidity("As senhas não conferem!");
+} else {
+  confirmaSenha.setCustomValidity("");
+}
+}
+
+senha.onchange = validaSenha;
+confirmaSenha.onkeyup = validaSenha;
