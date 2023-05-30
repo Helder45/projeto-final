1c9b71c3db0374be4b3d3ae743137ce742add604
Helder45 <helder.pereira@estudante.ifms.edu.br>
2023-05-29 22:30:03 -0400

    changes made

--- js/validar.js
+++ js/validar.js
@@ -1,6 +1,7 @@
 var senha = document.getElementById("senha"),
 confirmaSenha = document.getElementById("confirmaSenha");
 
+
 function validaSenha() {
 if (senha.value != confirmaSenha.value) {
   confirmaSenha.setCustomValidity("As senhas não conferem!");
